bb1cab7be107516c3c8ad25101ec53ce74c2da73
NehaMJ04 <jayannehamary@gmail.com>
2025-02-24 11:41:42 +0530

    Update streamlit_car_sales.py

--- streamlit_car_sales.py
+++ streamlit_car_sales.py
@@ -48,13 +48,7 @@ if page == "Data Analysis":
             color=top_brands.index,  # Use color for better distinction
             color_discrete_sequence=plotly.colors.qualitative.Plotly # Colorblind-friendly palette
         )
-        fig.update_yaxes(
-            range=[4900, top_brands.max() + 10],  # Set the y-axis range (min, max)
-            title="Number of Cars",  # Update y-axis title
-            showgrid=True,  # Show gridlines
-            gridcolor="lightgray",  # Customize gridline color
-            dtick=10  # Set the interval between ticks (e.g., every 10 units)
-        )
+        
         st.plotly_chart(fig, use_container_width=True)
     
     elif selected_analysis == "Brand and Models":
